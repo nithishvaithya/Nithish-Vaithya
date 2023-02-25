989e21c7e445de3eb313feed4aae35eb28d42fb4
nithishvaithya <35093789+nithishvaithya@users.noreply.github.com>
2023-02-25 07:20:32 +0000

    GOOD PAIR

---
+++ Good Pair/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+
+public class Solution {
+    public int solve(int[] A, int B) {
+        for(int i=0;i<A.length;i++){
+            for(int j=i+1;j<A.length;j++){
+                if(A[i]+A[j] == B){
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] A = {1,2,3,4};
+        System.out.println(s.solve(A, 7));
+    }
+}
